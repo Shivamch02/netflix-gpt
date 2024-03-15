4ca781ff7f9932870a696be4d91301ea306e4f98
Shivam Chaurasiya <chshivam53@gmail.com>
2024-03-15 13:47:00 +0530

    add VideoBackground

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -9,10 +9,7 @@ const VideoBackground = ({ movieId }) => {
     <div className="w-screen">
       <iframe
         className="w-screen aspect-video"
-        src={
-          `https://www.youtube-nocookie.com/embed/${trailerVideo?.key}`
-          //+"?&autoplay=1&mute=1&controls=0&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1"
-        }
+        src={`https://www.youtube-nocookie.com/embed/${trailerVideo?.key}?&autoplay=1&mute=1&controls=0&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
